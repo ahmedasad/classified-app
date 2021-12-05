1485f1ba110b4fc918f61a3ff1ec98b7b0728e24
SyedAhmedUddin <syed.ahmed@findmyadventure.pk>
2021-12-05 18:59:19 +0500

    changes: - screenshots folder placement updated.

--- app/src/main/java/com/example/classifiedapp/ui/detailsfragment/DetailsViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.classifiedapp.ui.detailsfragment;
-
-import androidx.lifecycle.ViewModel;
-
-public class DetailsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/classifiedapp/ui/launchfragment/SplashViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.classifiedapp.ui.launchfragment
-
-import androidx.lifecycle.ViewModel
-
-class SplashViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
